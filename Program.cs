f4ce90a4f4e2961905ab51d6eb8383d08f594343
denisvyiser <40177715+denisvyiser@users.noreply.github.com>
2021-03-20 15:12:41 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace AzureBlobTest
         static async Task Main(string[] args)
         {
 
-            string connectionString = "DefaultEndpointsProtocol=https;AccountName=eventthumb20201219174901;AccountKey=Z1yN9gjZltd0E0f9u+FVCIUU2890L3VS2SpsYaK/9n46anFTAHXBtH+dwuIS0o058BhbX/UhJZ/AjpDaKgKFzg==;EndpointSuffix=core.windows.net";
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=event;AccountKey=Z1yN9gjZltd0E0f9u+FVCIVV2890L3VS2SpsYaK/9n46anFTAHXBtH+dwuIS0o058BhbX/UhJZ/AjpDaKgKFzg==;EndpointSuffix=core.windows.net";
             string containerName = "imagesapi";
-            string localFilePath = "C:\\Users\\Denis\\Desktop\\Tipos de Filas_RabbitMQ.png";
+            string localFilePath = "C:\\Tipos de Filas_RabbitMQ.png";
 
             //Upload from diretório
             string bloboname = await UploadBlob(connectionString, containerName, localFilePath);
